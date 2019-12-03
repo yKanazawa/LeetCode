ce0c9c3845a81b12062a91c7bd7c5442f114888d
yKanazawa <yakitori009@gmail.com>
2019-12-03 21:49:45 +0900

    0001.cpp

---
+++ 0001.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+    }
+};
+
+int main(void) {
+    Solution a;
+
+    vector<int> nums{ 2, 7, 11, 15 };
+    a.twoSum(nums, 9);
+}
+
