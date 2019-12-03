4c48be0a79b08fe083107ae726400b6fe55f8f88
yKanazawa <yakitori009@gmail.com>
2019-12-03 21:02:03 +0900

    0001.php

---
+++ 0001.php
@@ -0,0 +1,23 @@
+<?php
+
+$a = new Solution();
+print_r($a->twoSum([2,7, 11, 15], 9));
+
+class Solution {
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($nums, $target) {
+        for ($i = 0; $i < count($nums); $i++) {
+            for ($j = $i + 1; $j < count($nums); $j++) {
+                if ($nums[$i] + $nums[$j] == $target) {
+                    return [$i, $j];
+                }
+            }
+            
+        }
+        return null;
+    }
+}
